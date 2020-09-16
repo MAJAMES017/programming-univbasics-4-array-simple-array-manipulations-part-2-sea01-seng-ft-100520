dc8817ff9c7c1a28ce72cc17531af1eb810286a7
michael james <majames071@gmail.com>
2020-09-16 23:18:31 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def array_concat(array, array)
+  array.concat(array)
+end
+
+def using_insert(array, element)
+  array.sort
+end
+
+def using_uniq(array)
+  array.reverse
+end
+
+def using_flatten(array)
+ array.first
+end
+
+def using_delete(array, string)
+ array.last
+end
+
+def using_delete_at(array, integer)
+ array.size
+end
